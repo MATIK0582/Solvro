6668a43be913395fd2d65f9ccf8c70a99ffb4a98
MATIK0582 <matik0582@gmail.com>
2022-10-19 22:17:53 +0200

    Test commita

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/test', async (req, res) => {
     res.send('Hello World v2')
 })
 
+router.get('/test_dwa', async (req, res) => {
+    res.send('Hello World v3')
+})
+
 export default router
